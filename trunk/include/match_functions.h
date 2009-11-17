255aff36e93ef9a18b753b063c9fcd2de4ca726b
mdpianelli@gmail.com <mdpianelli@gmail.com@6805e7fa-d130-11de-bb02-e302c5db49d5>
2009-11-17 07:00:15 +0000

    Funcionan todos los listados

--- trunk/include/match_functions.h
+++ trunk/include/match_functions.h
@@ -5,19 +5,26 @@
 
 #define ARROW_TAG "<img alt=\"Redirect arrow without text.svg\" src=\"http://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Redirect_arrow_without_text.svg/30px-Redirect_arrow_without_text.svg.png\" width=\"30\" height=\"13\">"
 #define MAX_LENGTH 150
+#define TAG_LENGTH 10
 #define REF_CANT   20
-#define MAX_INDENT 10
+#define MAX_INDENT 50
+
+#define DEBUG FALSE
 
 
 typedef struct{
 	char text[MAX_LENGTH];
 	unsigned int level; //De 1 a MAX_INDENT;
-}indentRef;
+}listRef;
 
 typedef struct{
-	indentRef indents[MAX_LENGTH];
+	listRef matches[MAX_LENGTH];
+	char oTag[TAG_LENGTH];
+	char cTag[TAG_LENGTH];
+	char moTag[TAG_LENGTH];
+	char mcTag[TAG_LENGTH];
 	int index;
-}tIndent;
+}tList;
 
 
 typedef enum{FALSE, TRUE} boolean;
@@ -26,11 +33,11 @@ void matchItalic(FILE * out);
 void matchBold(FILE * out);
 void matchBoldItalic(FILE *out);
 void matchHeading(char *text, FILE *out);
-void matchIndent(tIndent * ref, FILE *out);
 void matchRedirect(char *text, FILE *out);
 void matchExtLink(char * link, char * text, FILE *out);
 void matchInterLink(char *text, char * title, FILE *out);
-void recursiveIndent(tIndent *ref,FILE *out,int index);
+void matchList(tList * ref, FILE *out);
+void recursiveList(tList *ref,FILE *out,int index);
 
 #endif /* MATCHFUNCTION_H_ */
 
