5869b275706a161a0b1b5dd680decce5bc77be57
cliksmodernos <cliksmodernos@6805e7fa-d130-11de-bb02-e302c5db49d5>
2010-06-26 00:09:05 +0000

    Casi casi!

--- 2010/tp2/src/main/java/parserll/PredictionTable.java
+++ 2010/tp2/src/main/java/parserll/PredictionTable.java
@@ -13,12 +13,38 @@ public class PredictionTable implements Table {
 	public PredictionTable(List<Production> productions){
 		
 		Symbol string, stack;
+		TableKey key;
+		
+		//fill the table with the productions 
+//		for(Production p : productions){
+//			stack = new Symbol(p.getLeftPart().getSymbol());
+//			string = new Symbol(p.getRightPart()[0].getSymbol());
+//			table.put(new TableKey(string, stack), p.getRightPart());
+//		}
 		
 		for(Production p : productions){
 			stack = new Symbol(p.getLeftPart().getSymbol());
 			string = new Symbol(p.getRightPart()[0].getSymbol());
+			key = new TableKey(string, stack); 
+			
+			//if it was already put (with a null value), remove it
+			if(table.get(key) != null)
+				table.remove(key);
+				
+			//insert the original
 			table.put(new TableKey(string, stack), p.getRightPart());
+			
+			//insert the rest of the values
+			for(int i = 1; i < p.getRightPart().length; i++){
+				string = new Symbol(p.getRightPart()[i].getSymbol());
+				key = new TableKey(string, stack);
+				
+				//in this case, ONLY if it's null put it...
+				if(table.get(key) == null)
+					table.put(key, Symbol.getArrayOfSymbols(""));
+			}
 		}
+
 	}
 	
 	@Override
@@ -38,6 +64,7 @@ public class PredictionTable implements Table {
 		if(ans == null)
 			throw new NoSuchElementException("There isn't a right part for this conbination");
 		
+		//if its a null value, return null, else the answer
 		return ans;
 	}
 }
--- 2010/tp2/src/main/java/parserll/SpecialSymbols.java
+++ 2010/tp2/src/main/java/parserll/SpecialSymbols.java
@@ -4,6 +4,6 @@ public interface SpecialSymbols {
 
 	public static final char END_OF_STRING_MARK = '#';
 	public static final char LAMBDA = '\\';
-	
+	public static final char[] NULL = {'\0'};
 	
 }
